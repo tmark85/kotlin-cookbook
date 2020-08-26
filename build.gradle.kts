8318dd58eb7ffaf8026a39d4c267c1a794e50a1e
tmark85 <41347308+tmark85@users.noreply.github.com>
2020-08-27 01:03:49 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.sonarqube") version ("3.0")
+  id "org.sonarqube" version "3.0"
     `java-library`
     kotlin("jvm") version "1.4.0"
     id("me.champeau.gradle.jmh") version "0.5.0"
